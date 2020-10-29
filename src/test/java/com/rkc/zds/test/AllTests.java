038fced88834bb8130372d19dc3d1f01956a145e
Richard Campion <campion.richard@gmail.com>
2020-10-29 11:54:47 -0400

    Testing

---
+++ src/test/java/com/rkc/zds/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.rkc.zds.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ContactControllerTest.class })
+public class AllTests {
+
+}
--- src/test/java/com/rkc/zds/test/ContactControllerTest.java
+++ src/test/java/com/rkc/zds/test/ContactControllerTest.java
@@ -1,8 +1,9 @@
 package com.rkc.zds.test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,17 +14,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-
 import com.rkc.zds.web.controller.ContactController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ContactController.class)
-class ContactControllerTest {
+public class ContactControllerTest {
+
+	public ContactControllerTest() {
+
+	}
 
 	@Autowired
 	private MockMvc mvc;
